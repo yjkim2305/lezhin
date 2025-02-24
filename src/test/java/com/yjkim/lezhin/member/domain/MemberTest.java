71446635cd7e7da89c7aaadfc93cfa4fdd9565e5
KimYoungJun <cyberact3181@gmail.com>
2025-02-24 14:53:33 +0900

    feat: member 도메인 비밀번호 검증 테스트

--- src/test/java/com/yjkim/lezhin/member/domain/MemberTest.java
+++ src/test/java/com/yjkim/lezhin/member/domain/MemberTest.java
@@ -1,5 +1,7 @@
 package com.yjkim.lezhin.member.domain;
 
+import com.yjkim.lezhin.common.exception.CoreException;
+import com.yjkim.lezhin.member.api.exception.MemberErrorType;
 import com.yjkim.lezhin.member.application.dto.MemberCreateCommand;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -7,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MemberTest {
 
@@ -28,6 +32,33 @@ class MemberTest {
         assertThat(member.getMemberName()).isEqualTo("테스터");
         assertThat(member.getBirthDate()).isEqualTo("1991-08-28");
         assertThat(bCryptPasswordEncoder.matches("securePassword", member.getPassword())).isTrue();
+    }
+
+    @Test
+    @DisplayName("비밀번호가 올바르면 검증을 통과한다.")
+    void validatePassword_Success() {
+        MemberCreateCommand memberCreateCommand = new MemberCreateCommand("test@kidaristudio.com", "securePassword", "테스터", "19910828");
+        Member member = Member.createWithEncodedPassword(memberCreateCommand, bCryptPasswordEncoder);
+
+        assertDoesNotThrow(() -> member.validatePassword("securePassword", bCryptPasswordEncoder));
+    }
+
+    @Test
+    @DisplayName("비밀번호가 올바르지 않으면 검증에서 예외가 발생한다.")
+    void validatePassword_Fail() {
+        Member member = Member.builder()
+                .memberEmail("test@kidaristudio.com")
+                .memberName("test")
+                .password("password")
+                .build();
+
+        CoreException exception = assertThrows(CoreException.class, ()
+                -> member.validatePassword("password", bCryptPasswordEncoder)
+        );
+
+        assertThat(MemberErrorType.NOT_EXIST_USER_PASSWORD.getMessage()).isEqualTo(exception.getMessage());
+        assertThat(MemberErrorType.NOT_EXIST_USER_PASSWORD.name()).isEqualTo(exception.getErrorType().getErrorCode());
+
     }
 
 }
