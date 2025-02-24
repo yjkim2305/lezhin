458b4eb894653737d38a7d35b8754c799d831b8c
KimYoungJun <cyberact3181@gmail.com>
2025-02-24 09:35:31 +0900

    feat: 프로젝트 초기셋팅

---
+++ src/main/java/com/szs/lezhin/LezhinApplication.java
@@ -0,0 +1,13 @@
+package com.szs.lezhin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LezhinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LezhinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/szs/lezhin/LezhinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szs.lezhin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LezhinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
