8569561d31bf0099f48736819b8a74e771938097
KimYoungJun <cyberact3181@gmail.com>
2025-02-25 16:12:31 +0900

    feat: 사용자가 구매한 작품에 대한 도메인 entity 추가

---
+++ src/main/java/com/yjkim/lezhin/memberContent/domain/MemberContent.java
@@ -0,0 +1,20 @@
+package com.yjkim.lezhin.memberContent.domain;
+
+import com.yjkim.lezhin.content.domain.enums.PriceType;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberContent {
+    private Long id;
+    private Long memberId;
+    private Long contentId;
+    private PriceType priceType;  //사용자가 작품을 구매했을 때 무료, 유료인지 상태
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+}
---
+++ src/main/java/com/yjkim/lezhin/memberContent/infrastructure/entity/MemberContentEntity.java
@@ -0,0 +1,45 @@
+package com.yjkim.lezhin.memberContent.infrastructure.entity;
+
+import com.yjkim.lezhin.common.domain.entity.BaseTimeEntity;
+import com.yjkim.lezhin.content.domain.enums.PriceType;
+import com.yjkim.lezhin.memberContent.domain.MemberContent;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member_content")
+public class MemberContentEntity extends BaseTimeEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private Long contentId;
+
+    @Enumerated(EnumType.STRING)
+    private PriceType priceType;
+
+    @Builder
+    private MemberContentEntity(Long id, Long memberId, Long contentId, PriceType priceType) {
+        this.id = id;
+        this.memberId = memberId;
+        this.contentId = contentId;
+        this.priceType = priceType;
+    }
+
+    public static MemberContentEntity toEntity(MemberContent memberContent) {
+        return MemberContentEntity.builder()
+                .id(memberContent.getId())
+                .memberId(memberContent.getMemberId())
+                .contentId(memberContent.getContentId())
+                .priceType(memberContent.getPriceType())
+                .build();
+    }
+}
